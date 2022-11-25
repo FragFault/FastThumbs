570a3aca1ba9c0947398b04f012b3d8568a34ca7
Elijah Cozier <Ecozier@hotmail.com>
2022-11-25 15:45:17 -0500

    Navbar completed on settings branch rip

---
+++ app/src/main/java/com/example/simpletodo/fragments/LeaderboardFragment.kt
@@ -0,0 +1,28 @@
+package com.example.simpletodo.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.simpletodo.R
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [LeaderboardFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class LeaderboardFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
