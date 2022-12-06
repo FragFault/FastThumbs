6e418beb48c9cfb86bfa5ccbb4af18af8027995b
Dawidbtc <chudzikdavid1337@gmail.com>
2022-12-05 19:39:51 -0500

    LeaderboardFunctionality

--- app/src/main/java/com/example/simpletodo/fragments/LeaderboardFragment.kt
+++ app/src/main/java/com/example/simpletodo/fragments/LeaderboardFragment.kt
@@ -10,12 +10,14 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.simpletodo.BoardAdapter
 import com.example.simpletodo.Player
 import com.example.simpletodo.R
 import com.parse.FindCallback
 import com.parse.ParseException
 import com.parse.ParseQuery
+import java.sql.Types.NULL
 
 /**
  * A simple [Fragment] subclass.
@@ -27,6 +29,7 @@ class LeaderboardFragment : Fragment() {
     lateinit var adapter: BoardAdapter
 
     var allPlayers: MutableList<Player> = mutableListOf()
+    var threePlayers: MutableList<Player> = mutableListOf()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -41,29 +44,56 @@ class LeaderboardFragment : Fragment() {
         adapter = BoardAdapter(requireContext(),allPlayers)
         boardRecyclerView.adapter = adapter
         boardRecyclerView.layoutManager = LinearLayoutManager(requireContext())
-        queryBoard()
+        val firstPicture = view.findViewById<ImageView>(R.id.firstplace)
+        val firstName = view.findViewById<TextView>(R.id.textView4)
+        val firstPoints = view.findViewById<TextView>(R.id.textView5)
+        val secondPicture = view.findViewById<ImageView>(R.id.imageView7)
+        val secondName = view.findViewById<TextView>(R.id.textView8)
+        val secondPoints = view.findViewById<TextView>(R.id.textView9)
+        val thirdPicture = view.findViewById<ImageView>(R.id.imageView8)
+        val thirdName = view.findViewById<TextView>(R.id.textView10)
+        val thirdPoints = view.findViewById<TextView>(R.id.textView11)
+        queryBoard(firstPicture,secondPicture,thirdPicture,firstName,firstPoints,secondName,secondPoints,thirdName,thirdPoints)
+        Log.e("Leaderboardfragment:",""+threePlayers.size)
+        // val firstPicture = view.findViewById<ImageView>(R.id.firstplace)
+        //Glide.with(firstPicture.context).load(list.get(0).getPImage()?.url).circleCrop().into(firstPicture)
     }
 
-    fun queryBoard(){
+    fun queryBoard(firstPicture: ImageView, secondPicture: ImageView, thirdPicture: ImageView, firstName: TextView, firstPoints: TextView, secondName: TextView, secondPoints: TextView, thirdName: TextView, thirdPoints: TextView) {
         val query: ParseQuery<Player> = ParseQuery.getQuery(Player::class.java)
         query.include(Player.KEY_USER)
+        query.orderByDescending("totalPoints")
         query.findInBackground(object: FindCallback<Player>{
             override fun done(players: MutableList<Player>?, e: ParseException?) {
-                    if(e != null){
-                        Log.e("LeaderboarFragment:","Error fetching users")
-                    }else{
-                        if (players != null){
-                            Log.e("yo:",players.size.toString())
-                            for (player in players){
-                                Log.i("LeaderboardFragment:","Player:"+player.getBio()!!)
-                            }
-
-                            allPlayers.addAll(players)
-                            adapter.notifyDataSetChanged()
-                        }else{
-                            Log.i("LeaderboardFragment:","Players is null")
+                if(e != null){
+                    Log.e("LeaderboarFragment:","Error fetching users"+e)
+                }else{
+                    if (players != null){
+                        if(players.size>0) {
+                            Glide.with(firstPicture.context)
+                                .load(players.get(0).getPImage()?.url).circleCrop()
+                                .into(firstPicture)
+                            firstName.text = players.get(0).getUser()?.username as String
+                            firstPoints.text = players.get(0).getTotal().toString() + " Pts"
                         }
+                        if(players.size>1){
+                            Glide.with(firstPicture.context).load(players.get(1).getPImage()?.url).circleCrop().into(secondPicture)
+                            secondName.text = players.get(1).getUser()?.username as String
+                            secondPoints.text = players.get(1).getTotal().toString()+" Pts"
+                        }
+                        if(players.size>2){
+                            Glide.with(firstPicture.context).load(players.get(2).getPImage()?.url).circleCrop().into(thirdPicture)
+                            thirdName.text = players.get(2).getUser()?.username as String
+                            thirdPoints.text = players.get(2).getTotal().toString()+" Pts"
+                        }
+                        if(players.size>3) {
+                            allPlayers.addAll(players.slice(3 .. players.size-1 ))
+                            adapter.notifyDataSetChanged()
+                        }
+                    }else{
+                        Log.i("LeaderboardFragment:","Players is null")
                     }
+                }
             }
 
         })
