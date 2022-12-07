b7c4f080c21194f75a28d925be8a474769e5b935
Elijah Cozier <Ecozier@hotmail.com>
2022-12-06 23:08:41 -0500

    Test

--- app/src/main/java/com/example/simpletodo/CarouselRVAdapter.kt
+++ app/src/main/java/com/example/simpletodo/CarouselRVAdapter.kt
@@ -12,6 +12,7 @@ import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import com.example.simpletodo.PlayActivity
 import com.example.simpletodo.R
 import com.example.simpletodo.fragments.SelectionFragment
+import okhttp3.OkHttpClient
 import org.json.JSONException
 
 //Similar to a recycler view a carousel adapter is created
@@ -37,7 +38,7 @@ class CarouselRVAdapter(private val carouselDataList: Map<String, Drawable>, pri
     }
 
     inner class CarouselItemViewHolder(view: View) : RecyclerView.ViewHolder(view), View.OnClickListener { //Made the item View holder as an inner class to listen for clicks on each recyclerview item
-        private val LYRICS_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"
+        private val LYRICS_URL = "http://52/.87/.203/.225/api/getPoetryPrompt"
         init {
             itemView.setOnClickListener(this)
         }
@@ -53,7 +54,7 @@ class CarouselRVAdapter(private val carouselDataList: Map<String, Drawable>, pri
     }
 
     fun dataRetrieve(CATEGORY_URL: String, selection: String) {
-        val client = AsyncHttpClient()
+        val client = OkHttpClient()
         var INDEX = 0
 
         if(selection == "Poetry"){
@@ -66,12 +67,12 @@ class CarouselRVAdapter(private val carouselDataList: Map<String, Drawable>, pri
 
         client.get(CATEGORY_URL, object: JsonHttpResponseHandler() {
 
-            override fun onSuccess(statusCode: Int, headers: okhttp3.Headers?, json: JSON) {
+            override fun onSuccess(statusCode: Int, headers: okhttp3.Headers?, json: JSON?) {
                 Log.i(SelectionFragment.TAG, "onSuccess: JSON data $json")
                 try {
-                    val promptJsonArray = json.jsonObject.getJSONArray("results")
-                    val prompt = promptJsonArray.getJSONObject(INDEX)
-                    val theString =  prompt.getString("overview")
+                    val promptJsonArray = json?.jsonObject?.getJSONArray("results")
+                    val prompt = promptJsonArray?.getJSONObject(INDEX)
+                    val theString = prompt?.getString("overview")
                     Log.i(SelectionFragment.TAG, "The prompt: $theString")
 
                     //Add Data to Play Activity
@@ -105,3 +106,6 @@ class CarouselRVAdapter(private val carouselDataList: Map<String, Drawable>, pri
 
 
 }
+
+
+
