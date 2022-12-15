aaa6753643b6e0d1b4f431bdeec7a7806dda83a6
brandongranucci <brandongranucci00@gmail.com>
2022-12-15 11:17:11 -0500

    Comp, casual, and daily challenge functionality, new pink color theme, new parse object addition

--- app/src/main/java/com/example/simpletodo/ResultActivity.kt
+++ app/src/main/java/com/example/simpletodo/ResultActivity.kt
@@ -3,18 +3,59 @@ package com.example.simpletodo
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.text.SpannableStringBuilder
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.parse.ParseQuery
 import com.parse.ParseUser
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class ResultActivity: AppCompatActivity() {
+    val user = ParseUser.getCurrentUser()!!
+    var dailyPlayed = false
+    var speed = 0
+    var accuracy = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.end_results)
+
+
+        var speedRes = findViewById<TextView>(R.id.speedRes)
+        var accRes = findViewById<TextView>(R.id.accRes)
+        var dailyCText = findViewById<TextView>(R.id.dailyCText)
+
+        val extras = intent.extras
+        if (extras != null) {
+            //The key arguments here must match that used in the other activity
+
+            var extractedBoolean = extras.getBoolean("isDaily")
+            dailyPlayed = extractedBoolean
+            if (extractedBoolean) {
+                dailyCText.text = "1/1"
+            }
+
+            checkIfDailyPlayed()
+            if(dailyPlayed) {
+                dailyCText.text = "1/1"
+            }
+
+            var extractedInt = extras.getInt("speed")
+            speed = extractedInt
+            speedRes.text = speed!!.toString() + "WPM"
+
+            extractedInt = extras.getInt("accuracy")
+            accuracy = extractedInt
+            accRes.text = accuracy.toString() + "%"
+        }
+        else {
+            Toast.makeText(this, "Error: Couldn't get played game results!", Toast.LENGTH_SHORT).show()
+        }
 
         val conBtn = findViewById<Button>(R.id.conBtn).setOnClickListener {
             goToMainActivity()
@@ -22,20 +63,11 @@ class ResultActivity: AppCompatActivity() {
         val shareBtn = findViewById<Button>(R.id.shareBtn).setOnClickListener{
             shareOnTwitter()
         }
-
-        queryPosts()
     }
 
+    /*
     //query for all gamelogs testing
     fun queryPosts() {
-        //gets current user
-        val currentUser = ParseUser.getCurrentUser()
-        if (currentUser != null) {
-            Log.i(TAG, "currentUser: " + currentUser.objectId )
-        } else {
-            Log.i(TAG, "There was an error retrieving current user")
-        }
-
         //specify which class to query in data base
         val query = ParseQuery.getQuery(GameResults::class.java)
         //find all gamelogs in our server
@@ -59,7 +91,40 @@ class ResultActivity: AppCompatActivity() {
                     }
                 }
         }
+        */
 
+    fun checkIfDailyPlayed() {
+        val sdf = SimpleDateFormat("M/dd/yyyy")
+        var todaysDate = sdf.format(Date())
+        todaysDate = todaysDate.toString()
+
+        var parseDate = todaysDate.toString()
+
+        val query = ParseQuery.getQuery(GameResults::class.java)
+        query.whereEqualTo(GameResults.KEY_USER, user)
+        query.orderByDescending("updatedAt")
+        query.findInBackground { gamelogs, e ->
+            if (e != null) {
+                //Something when wrong getting the game logs
+            } else {
+                if (gamelogs != null) {
+                    for (gamelog in gamelogs) {
+                        parseDate = (sdf.format(gamelog.createdAt)).toString()
+                        if (parseDate == todaysDate) {
+                            if (gamelog.getDaily() == true) {
+                                dailyPlayed = true
+                                var dailyCText = findViewById<TextView>(R.id.dailyCText)
+                                dailyCText.text = "1/1"
+                                break
+                            }
+                        }
+                        else {
+                            break
+                        }
+                    }
+                }
+            }
+        }
     }
 
     private fun goToMainActivity() {
--- app/src/main/java/com/example/simpletodo/fragments/SelectionFragment.kt
+++ app/src/main/java/com/example/simpletodo/fragments/SelectionFragment.kt
@@ -10,9 +10,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.Switch
-import android.widget.TextView
+import android.widget.*
 import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
@@ -23,6 +21,7 @@ import com.bumptech.glide.Glide
 import com.example.simpletodo.PlayActivity
 import com.example.simpletodo.R
 import com.bumptech.glide.load.resource.bitmap.CircleCrop
+import com.example.simpletodo.GameResults
 import com.example.simpletodo.Player
 //import com.codepath.asynchttpclient.AsyncHttpClient
 //import com.codepath.asynchttpclient.RequestParams
@@ -36,6 +35,8 @@ import okhttp3.OkHttpClient
 import okhttp3.Response
 import org.json.JSONObject
 import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 import java.util.concurrent.TimeUnit
 
 
@@ -47,11 +48,15 @@ class SelectionFragment : Fragment() {
     lateinit var profilePic : ImageView
     lateinit var user : ParseUser
 
+    var dontAllowDaily = false
+    var isCompetitive = false
+
+
     // TODO: Rename and change types of parameters
 
     companion object {
         const val TAG = "SelectionScreenMessages"
-        @JvmStatic var isCompetetive: Boolean = false
+        //@JvmStatic var isCompetitive: Boolean = false
     }
 
     override fun onCreateView(
@@ -81,6 +86,15 @@ class SelectionFragment : Fragment() {
         user = ParseUser.getCurrentUser()
         profiledetails()
 
+        var tvModes = view.findViewById<TextView>(R.id.tvModes)
+        retrieveIsCompetitive(user)
+        if(isCompetitive) {
+            tvModes.text = "Selected Mode: Competitive"
+        }
+        else {
+            tvModes.text = "Selected Mode: Casual"
+        }
+
         val titleString = "Let's Type"
 
 
@@ -96,21 +110,65 @@ class SelectionFragment : Fragment() {
             prompt_Data = MOVIE_URL
         }
 
+        view.findViewById<Button>(R.id.btnSwitchModes).setOnClickListener {
+            isCompetitive = !isCompetitive
+            if(isCompetitive) {
+                tvModes.text = "Selected Mode: Competitive"
+            }
+            else {
+                tvModes.text = "Selected Mode: Casual"
+            }
+            updateMode()
+        }
+        /*
         view.findViewById<Switch>(R.id.modeSwitch).setOnClickListener {
-            isCompetetive = !isCompetetive
-//            Toast.makeText(requireContext(), "Switced Modes $isCompetetive", Toast.LENGTH_SHORT).show()
+            isCompetitive = !isCompetitive
+            Toast.makeText(requireContext(), "Switced Modes $isCompetitive", Toast.LENGTH_SHORT).show()
 
         }
+        */
 
         titleView.text = titleString + " " + player + "!"
+
         ivDaily.setOnClickListener(View.OnClickListener {
+            dontAllowDaily = false
+            val sdf = SimpleDateFormat("M/dd/yyyy")
+            var todaysDate = sdf.format(Date())
+            todaysDate = todaysDate.toString()
 
-            dataRetrieve(prompt_Data, "Movies")
-            if (it != null) {
-                it.setOnClickListener(null)
+            var parseDate = todaysDate.toString()
+
+            val query = ParseQuery.getQuery(GameResults::class.java)
+            query.whereEqualTo(GameResults.KEY_USER, user)
+            query.orderByDescending("updatedAt")
+            query.findInBackground { gamelogs, e ->
+                if (e != null) {
+                    //Something when wrong getting the game logs
+                } else {
+                    if (gamelogs != null) {
+                        for (gamelog in gamelogs) {
+                            parseDate = (sdf.format(gamelog.createdAt)).toString()
+                            if (parseDate == todaysDate) {
+                                if (gamelog.getDaily() == true) {
+                                    dontAllowDaily = true
+                                    Toast.makeText(context, "Daily Challenge Already Played Today!", Toast.LENGTH_SHORT).show()
+                                    break
+                                }
+                            }
+                            else {
+                                break
+                            }
+                        }
+                    }
+                }
             }
-
-        })
+            if(dontAllowDaily == false) {
+                dataRetrieve(prompt_Data, "Movies")
+                if (it != null) {
+                    it.setOnClickListener(null)
+                }
+            }
+            })
 
         viewPager.apply {
             clipChildren = false  // No clipping the left and right items
@@ -128,11 +186,11 @@ class SelectionFragment : Fragment() {
             )
 
         viewPager.adapter = requireContext()?.let {
-//            isCompetetive = !isCompetetive
+//            isCompetitive = !isCompetitive
 
-            Log.i(TAG, isCompetetive.toString())//Pass Context as a parameter for switching intents
+            Log.i(TAG, isCompetitive.toString())//Pass Context as a parameter for switching intents
             CarouselRVAdapter(demoData as Map<String, Drawable>,
-                it, isCompetetive
+                it, isCompetitive
             )
         }
 
@@ -201,14 +259,51 @@ class SelectionFragment : Fragment() {
                     val jObject = JSONObject(body)
                     val intent = Intent(context,PlayActivity::class.java)
                     intent.putExtra("prompt",jObject.get("prompt").toString())
+                    intent.putExtra("category", "none")
+                    intent.putExtra("isCompetitive", true)
+                    intent.putExtra("isDaily", true)
                     context?.startActivity(intent)
-
                 }
             }
         })
-
     }
 
+    fun retrieveIsCompetitive(user: ParseUser) {
+        val query = ParseQuery.getQuery(Player::class.java)
+        query.whereEqualTo(Player.KEY_USER, user)
+        query.findInBackground { playerObjects , e ->
+            if (e == null) {
+                for (playerObject in playerObjects) {
+                    isCompetitive = playerObject.getIsCompetitive()!!
+                }
+            } else {
+                Log.i(TAG, "Error: Failure to query user stats in Players table")
+            }
+        }
+    }
+
+    fun updateMode() {
+        val query = ParseQuery.getQuery(Player::class.java)
+        query.whereEqualTo(Player.KEY_USER, user)
+        query.findInBackground { playerObjects , e ->
+            if (e == null) {
+                for (playerObject in playerObjects) {
+                    playerObject.setIsCompetitve(isCompetitive!!)
+                    playerObject.saveInBackground { exception ->
+                        if (exception != null) {
+                            // Something has gone wrong
+                            Log.i(TAG, "Error: Failure to save mode preference to database!")
+                        }
+                        else {
+                            // No error saving to Players table
+                        }
+                    }
+                }
+            } else {
+                Log.i(TAG, "Error: Failure to query player preference in Players Table!")
+            }
+        }
+    }
 
 }
 
